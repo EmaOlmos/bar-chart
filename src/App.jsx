d21ad68bca5c657c93f7d59137fa8cc3cc642d95
EmaOlmos <eduardoolmosdionisio@gmai.com>
2024-04-04 23:23:19 -0600

    finally, this is it, the end, I'm so happy omg

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,36 @@ function App() {
       fetchData(url);
       setFetched(true);
     } else {
+      d3.select(svg)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -250)
+        .attr("y", 70)
+        .text("Gross Domestic Product");
+
+      const tooltip = d3
+        .select(".svgWrapper")
+        .append("div")
+        .attr("id", "tooltip")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+      const overlay = d3
+        .select(".svgWrapper")
+        .append("div")
+        .attr("class", "overlay")
+        .style("opacity", 0);
+
       const dates = theData.map((e) => {
         return new Date(e[0]);
+      });
+
+      const years = theData.map((e) => {
+        return e[0].slice(0, 4);
+      });
+
+      const gdp = theData.map((e) => {
+        return e[1];
       });
 
       const dataMax = d3.max(theData, (d) => d[1]);
@@ -49,25 +77,59 @@ function App() {
         .attr("height", height)
         .attr("width", width)
         .append("g")
+        .attr("id", "x-axis")
         .attr("transform", `translate(0, ${height - margin})`)
         .call(d3.axisBottom(x));
 
-      const y = d3.scaleLinear([0, dataMax], [height - margin, 10]); // left axis
+      // left axis
+      const y = d3.scaleLinear([0, dataMax], [height - margin, 10]);
       d3.select(svg)
         .append("g")
+        .attr("id", "y-axis")
         .attr("transform", `translate(${margin}, 0)`)
         .call(d3.axisLeft(y));
 
+      // create rectangles to represent data
       d3.select(svg)
         .selectAll("rect")
         .data(theData)
         .join("rect")
-        .attr("width", 3)
+        .attr("width", width / 275)
         .attr("height", (d) => height - margin - y(d[1]))
         .attr("x", (d) => x(new Date(d[0])))
         .attr("y", (d) => y(d[1]))
         .attr("fill", "burlywood")
-        .attr("class", "bars");
+        .attr("class", "bar")
+        .attr("index", (d, i) => i)
+        .on("mouseover", (e, d) => {
+          let i = e.currentTarget.getAttribute("index");
+          const mouseX = e.pageX;
+          const mouseY = e.pageY;
+
+          overlay // overlay for tooltip
+            .transition()
+            .duration(0)
+            .style("height", d + "px")
+            .style("width", width / 275 + "px")
+            .style("opacity", 0.8)
+            .style("left", mouseX + "px")
+            .style("top", height - d + "px")
+            .style("transform", "translateX(-100px)");
+
+          tooltip.transition().duration(200).style("opacity", 0.8); // tooltip following mouse and displaying data
+          tooltip
+            .html(years[i] + "<br>" + "$" + gdp[i] + " Billion")
+            .attr("data-date", theData[i][0])
+            .attr("data-gdp", theData[i][1])
+            .style("left", mouseX + 10 + "px")
+            .style("top", mouseY - 80 + "px")
+            .style("transform", "translateX(-100px)");
+        })
+        // on mouse out, hide overlay and tooltip
+        .on("mouseout", () => {
+          tooltip.transition().duration(200).style("opacity", 0);
+          overlay.transition().duration(200).style("opacity", 0);
+        });
     }
   }, [theData]);
 
@@ -78,9 +140,19 @@ function App() {
           <div className="lilWrapper">
             <h1 id="title">Data Bar Chart</h1>
             <svg ref={svgRef}></svg>
-            <div className="infoWrapper">
-              <h3></h3>
-            </div>
+            <h4 id="link">
+              more information at{" "}
+              <span>
+                <a
+                  href="http://www.bea.gov/national/pdf/nipaguid.pdf"
+                  target="_blank"
+                >
+                  http://www.bea.gov/national/pdf/nipaguid.pdf
+                </a>
+              </span>
+            </h4>
+            <br />
+            <h3>Made By 🍒</h3>
           </div>
         </div>
       </div>
